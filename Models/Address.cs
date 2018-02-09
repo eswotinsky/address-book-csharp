8d7acdb30cc9c6059d7182a22b89eeaef0b0c4b5
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-02-09 11:07:59 -0800

    add Address class; minor Index.cshtml styling.

---
+++ Models/Address.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook.Models
+{
+  public class Address
+  {
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _zipCode;
+
+    public Address (string street, string city, string state, string zipCode)
+    {
+      _street = street;
+      _city = city;
+      _state = state;
+      _zipCode = zipCode;
+    }
+
+    public string GetStreet()
+    {
+      return _street;
+    }
+
+    public string GetCity()
+    {
+      return _city;
+    }
+
+    public string GetState()
+    {
+      return _state;
+    }
+
+    public string GetZipCode()
+    {
+      return _zipCode;
+    }
+
+  }
+}
